f32844f5413a256005fc5d2260394707a5d34e42
Giuseppe Verni <gverni@stripe.com>
2023-10-27 18:28:47 +0100

    Include stripe into function

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,3 +1,12 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2020-08-27',
+    appInfo: { // For sample support and debugging, not required for production:
+      name: "stripe-samples/accept-a-payment/payment-element",
+      version: "0.0.2",
+      url: "https://github.com/stripe-samples"
+    }
+  });
+
 exports.handler = async () => {
   // Create a PaymentIntent with the amount, currency, and a payment method type.
   //
